0f6db08e2b159a010e4b74bb1a34a7ea35f9d667
yanai <yanai@tikalk.com>
2018-02-19 16:11:01 +0200

    serverless

--- handler.js
+++ handler.js
@@ -10,51 +10,51 @@ const onError = (error, callback) => {
 }
 
 module.exports.getPlayersScores = (event, context, callback) => {
-    if(event.pathParameters.gameId === "111"){
+//    if(event.pathParameters.gameId === "111"){
         console.log('handling latest request');
         db.getLatestGame(event.pathParameters.gameId, (error, result) => {
             console.log('result is:'+result);
-//            const gameId = result.Items[0].gameId.N;
-//            console.log('found gameId:'+gameId);
-//
-//            db.getPlayerScores(gameId, (error, result) => {
-//                if (error) {
-//                    onError(error, callback);
-//                    return;
-//                }
-//
-//                // create a response
-//                const response = {
-//                    statusCode: 200,
-//                    headers: {
-//                        "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-//                        "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
-//                    },
-//                    body: JSON.stringify(result.Items),
-//                };
-//                callback(null, response);
-//            });
-        });
-    }
-    else{
-        db.getPlayerScores(event.pathParameters.gameId, (error, result) => {
-            if (error) {
-                onError(error, callback);
-                return;
-            }
+            const gameId = result.Items[0].gameId.N;
+            console.log('found gameId:'+gameId);
 
-            // create a response
-            const response = {
-                statusCode: 200,
-                headers: {
-                    "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-                    "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
-                },
-                body: JSON.stringify(result.Items),
-            };
-            callback(null, response);
+            db.getPlayerScores(gameId, (error, result) => {
+                if (error) {
+                    onError(error, callback);
+                    return;
+                }
+
+                // create a response
+                const response = {
+                    statusCode: 200,
+                    headers: {
+                        "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+                        "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+                    },
+                    body: JSON.stringify(result.Items),
+                };
+                callback(null, response);
+            });
         });
-    }
+//    }
+//    else{
+//        db.getPlayerScores(event.pathParameters.gameId, (error, result) => {
+//            if (error) {
+//                onError(error, callback);
+//                return;
+//            }
+//
+//            // create a response
+//            const response = {
+//                statusCode: 200,
+//                headers: {
+//                    "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+//                    "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+//                },
+//                body: JSON.stringify(result.Items),
+//            };
+//            callback(null, response);
+//        });
+//    }
 };
 
 module.exports.getTeamsScores = (event, context, callback) => {
